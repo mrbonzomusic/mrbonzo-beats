9c1fba34a1b2c1f754924bb6d445968c7a085a5f
mrbonzomusic <mrbonzomusic@gmail.com>
2026-04-08 17:37:33 +0300

    Auto-update

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,7 +2,7 @@ export const socialLinks = {
   spotify: "https://open.spotify.com/artist/your-id", // Βάλε το κανονικό σου Spotify link εδώ
   youtube: "https://www.youtube.com/@mrbonzobeatz",
   tiktok: "https://www.tiktok.com/@mr.bonzobeats",
-  instagram: "https://www.instagram.com/mrbonzobeatz/",
+  instagram: "https://www.instagram.com/mrbonzobeats/",
   discord: "https://discord.gg/YOUR_INVITE_LINK", // <--- Πρόσθεσε το δικό σου Discord link εδώ
   email: "mrbonzomusic@gmail.com" // Το κρατάμε καθαρό εδώ
 };
