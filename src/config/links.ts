106805eb4b64d5fb7c8f9704480fd9b584518810
mrbonzomusic <mrbonzomusic@gmail.com>
2026-05-14 13:07:49 +0300

    chore: update Discord invite to NjSeXpQHT6

--- src/config/links.ts
+++ src/config/links.ts
@@ -4,7 +4,7 @@ export const socialLinks = {
   youtube: "https://www.youtube.com/@mrbonzobeatz",
   tiktok: "https://www.tiktok.com/@mr.bonzobeats",
   instagram: "https://www.instagram.com/mrbonzobeats/",
-  discord: "https://discord.gg/hmpCHd4J", // Εδώ έβαλα αυτό που είδα στον κώδικά σου πριν
+  discord: "https://discord.gg/NjSeXpQHT6",
   
   // ΠΡΟΣΟΧΗ: Προσθέτουμε το mailto: εδώ για να δουλεύει απευθείας το κλικ
   email: "mailto:mrbonzomusic@gmail.com" 
